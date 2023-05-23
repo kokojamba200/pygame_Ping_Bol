a00b2f62f4b3ec76ef27d7720885d8c683af0988
Romause <roman.osatsky@mail.ru>
2023-05-23 18:40:47 +0300

    adding ball moving function

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import sys
 import pygame as pg
 
+
+def ball_move(obj):
+    global speed_x, speed_y
+    obj.x += speed_x
+    obj.y += speed_y
+
+    if obj.top <= 0 or obj.bottom >= H:
+        speed_y *= -1
+    elif obj.left <= 0 or obj.right >= W:
+        speed_x *= -1
+    elif obj.colliderect(player) or obj.colliderect(opponent):
+        speed_x *= -1
+
+
 W = 1280
 H = 720
 FPS = 60
@@ -19,9 +33,17 @@ opponent = pg.Rect(10, H // 2, 10, 150)
 ball = pg.Rect(W // 2 - 15, H // 2 - 10, 30, 30)
 
 
+speed = 7
+p_speed = 0
+o_speed = 0
+ball_moving = False
+speed_x = speed_y = speed
+
+
 pg.init()  # инициализируем pygame
 screen = pg.display.set_mode((W, H))  # создаем экран игры разрешением 1280х720px
 pg.display.set_caption('Ping Pong | PyGame')
+
 
 while True:  # цикл игры
     clock.tick(FPS)
@@ -36,4 +58,6 @@ while True:  # цикл игры
     pg.draw.aaline(screen, WHITE, [W // 2, 0], [W // 2, H])
     pg.draw.ellipse(screen, VIOLET, ball)
     pg.display.update()
+
+    ball_move(ball)
 
